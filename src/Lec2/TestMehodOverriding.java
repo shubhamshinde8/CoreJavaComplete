c45a96aa7586d7db898b9dd174b1af3797b252be
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-04-01 21:12:38 -1200

    save all changes

--- src/Lec2/TestMehodOverriding.java
+++ src/Lec2/TestMehodOverriding.java
@@ -1,6 +1,33 @@
 package Lec2;
 
-public class TestMehodOverriding {
+abstract class Calculator{
 
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public abstract void div(int a,int b);
+
+
+}
+
+class AdvCalculator extends Calculator{
+    @Override
+    public void div(int a,int b) {
+        System.out.println("Iam Abstract method from class Calculator "+(a/b));
+    }
+
+//    public int add(int a,int b){
+//        return a+b+1;
+//    }
+}
+
+
+public class TestMehodOverriding {
+    public static void main(String[] args) {
+        AdvCalculator obj=new AdvCalculator();
+        obj.div(10,20);
+        System.out.println(obj.add(2,3));
+    }
 
 }
